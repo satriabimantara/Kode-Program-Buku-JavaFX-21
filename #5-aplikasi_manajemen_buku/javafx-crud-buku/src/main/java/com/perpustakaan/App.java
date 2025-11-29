ee7c31ad940f5e2cf6ec6ea942d9b3cbecc8a7e0
satriabimantara <satriabimantara.md@gmail.com>
2025-11-29 21:48:36 +0800

    - Update JavaFX codes for Book CRUD Application MySQL
    - Already tested and successfully done

--- #5-aplikasi_manajemen_buku/javafx-crud-buku/src/main/java/com/perpustakaan/App.java
+++ #5-aplikasi_manajemen_buku/javafx-crud-buku/src/main/java/com/perpustakaan/App.java
@@ -17,9 +17,21 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        // / PASTIKAN NAMA FXML SESUAI
+        scene = new Scene(loadFXML("BukuView"), 700, 600); 
+        stage.setTitle("CRUD Buku JavaFX");
         stage.setScene(scene);
         stage.show();
+    }
+    @Override
+    public void stop() throws Exception {
+        // Memberikan konfirmasi bahwa siklus hidup aplikasi telah berakhir.
+        System.out.println("Aplikasi JavaFX telah dimatikan secara normal.");
+        
+        // Jika di masa depan Anda menambahkan service/thread background,
+        // kode penutupan/shutdown-nya akan diletakkan di sini.
+        
+        super.stop(); 
     }
 
     static void setRoot(String fxml) throws IOException {
